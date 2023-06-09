7d51850ec76455f1ee4f2e1a3a978d10c253da4a
Qiwenweiii <Qiwenweiii@foxmail.com>
2023-06-09 14:18:10 +0800

    Home页添加样式

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+// 全局注册所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <el-container class="container">
-    <el-aside :width="isCollapsed ? '75px' : '200px'" class="aside">
+    <el-aside :width="isCollapsed ? '64px' : '200px'" class="aside">
       <div class="logo">
-        <img src="@/assets/logo.svg" alt="">{{ isCollapsed ? '' : 'Qiwenweiii' }}
+        <img src="@/assets/logo.svg" alt="">
+        <transition name="fade">
+          <span v-if="!isCollapsed">Qiwenweiii</span>
+        </transition>
       </div>
       <el-menu default-active="1" :collapse="isCollapsed">
         <el-sub-menu index="1">
           <template #title>
-            <el-icon>
-              <location />
+            <el-icon :size="20">
+              <Location />
             </el-icon>
             <span>Navigator One</span>
           </template>
@@ -25,18 +28,20 @@
           </el-sub-menu>
         </el-sub-menu>
         <el-menu-item index="2">
-          <el-icon><icon-menu /></el-icon>
+          <el-icon>
+            <Menu />
+          </el-icon>
           <span>Navigator Two</span>
         </el-menu-item>
         <el-menu-item index="3">
           <el-icon>
-            <document />
+            <Document />
           </el-icon>
           <span>Navigator Three</span>
         </el-menu-item>
         <el-menu-item index="4">
           <el-icon>
-            <setting />
+            <Setting />
           </el-icon>
           <span>Navigator Four</span>
         </el-menu-item>
@@ -44,51 +49,61 @@
     </el-aside>
     <el-container>
       <el-header class="header">
+        <div class="tools" @click="handleCollapse">
+          <el-icon>
+            <Menu />
+          </el-icon>
+        </div>
         <div class="nav-bar">
-          <el-menu class="el-menu-demo" mode="horizontal">
-            <el-menu-item index="1">Processing Center</el-menu-item>
-            <el-sub-menu index="2">
-              <template #title>Workspace</template>
-              <el-menu-item index="2-1">item one</el-menu-item>
-              <el-menu-item index="2-2">item two</el-menu-item>
-              <el-menu-item index="2-3">item three</el-menu-item>
-              <el-sub-menu index="2-4">
-                <template #title>item four</template>
-                <el-menu-item index="2-4-1">item one</el-menu-item>
-                <el-menu-item index="2-4-2">item two</el-menu-item>
-                <el-menu-item index="2-4-3">item three</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-menu-item index="3" disabled>Info</el-menu-item>
-            <el-menu-item index="4">Orders</el-menu-item>
+          <el-menu class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-menu-item index="2">消息中心</el-menu-item>
+            <el-menu-item index="3">订单管理</el-menu-item>
           </el-menu>
         </div>
         <div class="user-info">
           <el-dropdown>
             <span class="el-dropdown-link">
-              Username
+              Username <el-avatar shape="square" :src="userAvatarSrc" />
             </span>
             <template #dropdown>
               <el-dropdown-menu>
-                <el-dropdown-item>Action 1</el-dropdown-item>
-                <el-dropdown-item>
-                  Action 2
-                </el-dropdown-item>
-                <el-dropdown-item>Action 3</el-dropdown-item>
-                <el-dropdown-item>Action 4</el-dropdown-item>
-                <el-dropdown-item>Action 5</el-dropdown-item>
+                <el-dropdown-item>我的消息</el-dropdown-item>
+                <el-dropdown-item>设置</el-dropdown-item>
+                <el-dropdown-item divided>退出登录</el-dropdown-item>
               </el-dropdown-menu>
             </template>
           </el-dropdown>
         </div>
       </el-header>
-      <el-main class="main"></el-main>
+      <el-main class="main">
+        <el-row class="bread-crumb">
+          <el-col :span="24">
+            <el-breadcrumb separator="/" class="bread-crumb__inner">
+              <el-breadcrumb-item>homepage</el-breadcrumb-item>
+            </el-breadcrumb>
+          </el-col>
+        </el-row>
+        <el-row class="content">
+          <el-col :span="24">
+            <router-view></router-view>
+          </el-col>
+        </el-row>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script setup>
-const isCollapsed = false
+import { ref } from 'vue';
+
+const isCollapsed = ref(false)
+const userAvatarSrc = ref('https://img.zcool.cn/community/01feb15d6de34da801211f9ea7f4a3.jpg@1280w_1l_2o_100sh.jpg')
+
+const handleCollapse = () => {
+  isCollapsed.value = !isCollapsed.value
+}
 </script>
 
 <style lang="scss" scoped>
@@ -97,16 +112,30 @@ const isCollapsed = false
 
   .aside {
     border-right: 1px solid #ccc;
+    transition: all .5s ease-in-out;
 
     .logo {
       height: 50px;
       display: flex;
-      justify-content: center;
+      // justify-content: center;
       align-items: center;
       gap: 10px;
+      padding-left: 10px;
+      background-color: #485460;
+      color: #fff;
 
       img {
         width: 35px;
+      }
+
+      .fade-enter-from,
+      .fade-leave-to {
+        opacity: 0;
+      }
+
+      .fade-enter-active,
+      .fade-leave-active {
+        transition: all .5s ease-in-out;
       }
     }
 
@@ -120,15 +149,64 @@ const isCollapsed = false
     display: flex;
     justify-content: space-between;
     align-items: center;
+    gap: 10px;
     border-bottom: 1px solid #ccc;
+    background-color: #485460;
+    color: #fff;
     overflow: hidden;
+
+    .el-menu {
+      background-color: transparent;
+      color: #fff;
+      border: none;
+      height: 50px;
+    }
+
+    .tools {
+      cursor: pointer;
+    }
+
+    .tools:hover {
+      color: #409eff
+    }
 
     .nav-bar {
       flex: 1;
 
-      .el-menu {
-        border: none;
-        background-color: none;
+      .el-menu li {
+        color: #fff;
+
+        .is_active {
+          border-bottom: 1px solid #ffd04b;
+        }
+      }
+
+    }
+
+    .user-info .el-dropdown-link {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 5px;
+      color: #fff;
+      cursor: pointer;
+      outline: none;
+    }
+  }
+
+  .main {
+    margin: 0;
+    padding: 0;
+
+    .bread-crumb {
+      height: 30px;
+      line-height: 30px;
+      padding: 0 10px;
+      border-bottom: 1px solid #ccc;
+      font-size: 0.9em;
+
+      .bread-crumb__inner {
+        line-height: 30px;
       }
     }
   }
